bd0cf1038757f71eae2a963e2c11a276b5bd75cd
Kyle <kyruken@gmail.com>
2023-11-16 15:43:38 -0800

    add player movement

---
+++ scenes/game/scripts/player_scripts/Idle.gd
@@ -0,0 +1,12 @@
+extends State
+class_name PlayerIdle
+
+@export var animator : AnimationPlayer
+
+func Enter():
+	owner.velocity = Vector2.ZERO
+	
+func Update(delta: float):
+	if (Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")
+	or Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down")):
+		Transitioned.emit(self, "walk")

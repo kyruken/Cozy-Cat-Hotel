e26baf04d045fdc1c6a9116a4f543a5d65288dde
Kyle <kyruken@gmail.com>
2023-11-16 19:07:00 -0800

    add physics, add left right movement

--- scenes/game/scripts/player_scripts/Walk.gd
+++ scenes/game/scripts/player_scripts/Walk.gd
@@ -13,13 +13,25 @@ func Update(delta: float):
 	pass
 
 func Physics_Update(delta: float):
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	owner.velocity = input_direction * speed
-	print(owner.velocity)
+	## Left to Right Movement ##
+	var input_direction_x: float = (
+		Input.get_action_strength("ui_right")
+		- Input.get_action_strength("ui_left")
+	)
+	player.velocity.x = player.speed * input_direction_x
+	player.velocity.y += player.gravity * delta
+	
 	flip_sprite()
 	
+	## Conditions for state changes ##
 	if (owner.velocity == Vector2.ZERO):
 		Transitioned.emit(self, "idle")
+		
+		
+	## Jump state if want to implement ##
+#	elif(Input.is_action_just_pressed("ui_up")):
+#		player.velocity.y -= player.jump_impulse
+#		print(player.velocity.y)
 		
 func flip_sprite():
 	if (owner.velocity.x < 0):
--- scenes/game/scripts/player_scripts/player.gd
+++ scenes/game/scripts/player_scripts/player.gd
@@ -1,4 +1,12 @@
 extends CharacterBody2D
 
+var speed := 500.0
+var jump_impulse := 1200.0
+var base_gravity := 4000.0
+var glide_gravity := 1000.0
+
+var _is_gliding := false
+var gravity := glide_gravity if _is_gliding else base_gravity
+
 func _physics_process(delta):
 	move_and_slide()

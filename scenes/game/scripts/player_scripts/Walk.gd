e6af5c7f88d41d9dc294abd671a0b5307c650c88
Kyle <kyruken@gmail.com>
2023-11-20 16:35:57 -0800

    refactor playerwalk state

--- scenes/game/scripts/player_scripts/Walk.gd
+++ scenes/game/scripts/player_scripts/Walk.gd
@@ -6,36 +6,38 @@ class_name PlayerWalk
 @export var sprite : Sprite2D
 @export var animator : AnimationPlayer
 
-func Enter():
-	pass
-	
-func Update(delta: float):
-	pass
-
 func Physics_Update(delta: float):
-	## Left to Right Movement ##
-	var input_direction_x: float = (
-		Input.get_action_strength("ui_right")
-		- Input.get_action_strength("ui_left")
-	)
-	player.velocity.x = player.speed * input_direction_x
-	player.velocity.y += player.gravity * delta
 	
+	move(delta, handle_input())
 	flip_sprite()
 	
+	check_for_state_transition()
+	
 	## Conditions for state changes ##
-	if (owner.velocity == Vector2.ZERO):
-		Transitioned.emit(self, "idle")
-		
-		
 	## Jump state if want to implement ##
 #	elif(Input.is_action_just_pressed("ui_up")):
 #		player.velocity.y -= player.jump_impulse
 #		print(player.velocity.y)
-		
+
+func handle_input():
+	var input_direction_x: float = (
+		Input.get_action_strength("ui_right")
+		- Input.get_action_strength("ui_left")
+	)
+	
+	return input_direction_x
+
+func move(delta, input):
+	player.velocity.x = player.speed * input
+	player.velocity.y += player.gravity * delta
+	
 func flip_sprite():
 	if (owner.velocity.x < 0):
 		sprite.flip_h = 0
 	elif (owner.velocity.x > 0):
 		sprite.flip_h = 1
-	
+
+func check_for_state_transition():
+	if (owner.velocity == Vector2.ZERO):
+		Transitioned.emit(self, "idle")
+

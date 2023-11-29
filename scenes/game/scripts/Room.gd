ea1c91884a54f59bb2ee09d69a7e462d771af615
Kyle <kyruken@gmail.com>
2023-11-29 14:33:14 -0800

    add progress bar to rooms

--- scenes/game/scripts/Room.gd
+++ scenes/game/scripts/Room.gd
@@ -16,6 +16,9 @@ var current_state: RoomState = RoomState.NO_CUSTOMER
 @onready var room_timer = $Timer
 @onready var textbox_area := $TextBoxArea2D
 @onready var location = self.global_position
+@onready var progress_bar = $ProgressBar
+
+var has_progress_bar_max = false
 
 func _process(delta):
 	match current_state:
@@ -23,6 +26,7 @@ func _process(delta):
 			if (customer_in_room):
 				current_state = RoomState.HAS_CUSTOMER
 		RoomState.HAS_CUSTOMER:
+			handle_progress_bar()
 			if (room_timer.is_stopped()):
 				textbox_area.visible = !textbox_area.visible 
 				current_state = RoomState.DIRTY_ROOM
@@ -43,7 +47,16 @@ func handle_dirty_room(body):
 		set_customer_in_room(null)
 		current_state = RoomState.NO_CUSTOMER
 		textbox_area.visible = !textbox_area.visible 
-		
+
+func handle_progress_bar():
+	if !has_progress_bar_max:
+		progress_bar.max_value = room_timer.get_wait_time()
+		has_progress_bar_max = true
+	progress_bar.value = room_timer.get_time_left()
+	
+	if progress_bar.value == 0:
+		has_progress_bar_max = false
+	
 func _on_text_box_area_2d_body_entered(body):
 	player = body
 	player_inside = true
